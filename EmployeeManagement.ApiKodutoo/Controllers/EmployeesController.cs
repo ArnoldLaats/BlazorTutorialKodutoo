39cf182ad06739f9e5bf8f48c2e31ed8fba72ca8
Kindogrec <40635978+Kindogrec@users.noreply.github.com>
2021-08-26 18:22:14 +0300

    EmployeesController and stuff

---
+++ EmployeeManagement.ApiKodutoo/Controllers/EmployeesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmployeeManagement.ApiKodutoo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.ApiKodutoo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeRepository employeeRepository;
+        public EmployeesController(IEmployeeRepository employeeRepistory)
+        {
+            this.employeeRepository = employeeRepistory;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetEmployees()
+        {
+            try
+            {
+                return Ok(await employeeRepository.GetEmployees());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCode.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
+    }
+}
