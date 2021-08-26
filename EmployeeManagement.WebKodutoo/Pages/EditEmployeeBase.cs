c2e4c0cbc4cd109c86a2c8f7b184bb2cb77083b7
Kindogrec <40635978+Kindogrec@users.noreply.github.com>
2021-08-26 20:18:12 +0300

    Episode 29

---
+++ EmployeeManagement.WebKodutoo/Pages/EditEmployeeBase.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeManagement.ModelsKodutoo;
+using EmployeeManagement.WebKodutoo.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.WebKodutoo.Pages
+{
+    public class EditEmployeeBase : ComponentBase
+    {
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+        public Employee Employee { get; set; } = new Employee();
+
+        [Parameter]
+        public string Id { get; set; }
+
+        protected async override Task OnInitalizedAsync()
+        {
+            Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+        }
+    }
+}
