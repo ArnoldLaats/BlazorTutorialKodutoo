3cab3aeced0890047ab30ea270a02e570643745d
Kindogrec <40635978+Kindogrec@users.noreply.github.com>
2021-08-26 17:10:00 +0300

    Add project files.

---
+++ EmployeeManagement.ModelsKodutoo/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManagement.ModelsKodutoo
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
+}
