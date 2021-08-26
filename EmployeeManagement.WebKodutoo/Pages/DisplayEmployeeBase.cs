e2a6bed765b07d06fe6bbaa16d97698129905f55
Kindogrec <40635978+Kindogrec@users.noreply.github.com>
2021-08-26 23:33:11 +0300

    Episode 44

--- EmployeeManagement.WebKodutoo/Pages/DisplayEmployeeBase.cs
+++ EmployeeManagement.WebKodutoo/Pages/DisplayEmployeeBase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EmployeeManagement.ModelsKodutoo;
+using EmployeeManagement.WebKodutoo.Services;
 using Microsoft.AspNetCore.Components;
 
 namespace EmployeeManagement.WebKodutoo.Pages
@@ -15,9 +16,20 @@ namespace EmployeeManagement.WebKodutoo.Pages
         public bool ShowFooter { get; set; }
         public EventCallback<bool> OnEmployeeSelection { get; set; }
 
+        public IEmployeeService EmployeeService { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
         protected async Task CheckBoxChanged(ChangeEventArgs e)
         {
             await OnEmployeeSelection.InvokeAsync((bool)e.Value);
+        }
+
+        protected async Task Delete_Click()
+        {
+            await EmployeeService.DeleteEmployee(Employee.EmployeeId);
+            NavigationManager.NavigateTo("/", true);
         }
     }
 }
