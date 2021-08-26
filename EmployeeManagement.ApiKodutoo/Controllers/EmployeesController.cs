3c477d0ba43e2e1c276f02cf8c3105b3012fe477
Kindogrec <40635978+Kindogrec@users.noreply.github.com>
2021-08-26 18:27:20 +0300

    Episode 15

--- EmployeeManagement.ApiKodutoo/Controllers/EmployeesController.cs
+++ EmployeeManagement.ApiKodutoo/Controllers/EmployeesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using EmployeeManagement.ApiKodutoo.Models;
 using System;
 using System.Collections.Generic;
@@ -23,6 +24,26 @@ namespace EmployeeManagement.ApiKodutoo.Controllers
             try
             {
                 return Ok(await employeeRepository.GetEmployees());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCode.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+            try
+            {
+                var result = await employeeRepository.GetEmployee(id);
+                if(result == null)
+                {
+                    return NotFound();
+                }
+
+                return result;
             }
             catch (Exception)
             {
