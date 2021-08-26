2c7b2e283ee2b2740617566a49724ff09f6ca9fe
Kindogrec <40635978+Kindogrec@users.noreply.github.com>
2021-08-26 17:26:53 +0300

    Some Employee Stuff

---
+++ EmployeeManagement.WebKodutoo/Pages/EmployeeListBase.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+using EmployeeManagement.ModelsKodutoo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.WebKodutoo.Pages
+{
+    public class EmployeeListBase : ComponentBase
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+            {
+                LoadEmployees();
+                return base.OnInitializedAsync();
+            }
+        }
+    }
+
+    private void LoadEmployees()
+    {
+        Employee e1 = new Employee
+        {
+            EmployeeId = 1,
+            FirstName = "John",
+            LastName = "Hastings",
+            Email = "David@pragimtech.com",
+            DateOfBirth = new DateTime(1980, 10, 5),
+            Gender = Gender.Male,
+            Department = new Department { DepartmentId = 1, DepartmentName = "Random" },
+            PhotoPath = "images/john.png"
+        }
+        Employee e2 = new Employee
+        {
+            EmployeeId = 1,
+            FirstName = "Wicky",
+            LastName = "Sasky",
+            Email = "SasWicky@dunno.com",
+            DateOfBirth = new DateTime(1992, 20, 7),
+            Gender = Gender.Female,
+            Department = new Department { DepartmentId = 2, DepartmentName = "Sasky" },
+            PhotoPath = "images/wicky.png"
+        }
+
+        Employees = new List<Employee> { e1, e2 };
+    }
+}
